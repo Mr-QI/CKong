73c78b2c9515f24420c82d0b91467a326af079b7
qds <qidongsheng90@gmail.com>
2021-01-16 19:02:59 +0800

    创建项目

---
+++ cmake-build-debug/CMakeFiles/CKong.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CKong.exe"
+  "CKong.exe.manifest"
+  "CKong.pdb"
+  "CMakeFiles/CKong.dir/main.c.obj"
+  "libCKong.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CKong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
